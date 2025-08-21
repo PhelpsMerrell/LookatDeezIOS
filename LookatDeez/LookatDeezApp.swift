8437c5a62aaf2403cbd72b5a499cb1344d1611f8
Phelps Merrell <phelps.tomas.merrell@gmail.com>
2025-08-20 20:28:46 -0600

    Initial Commit

---
+++ LookatDeez/LookatDeezApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookatDeezApp.swift
+//  LookatDeez
+//
+//  Created by Phelps Merrell on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LookatDeezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LookatDeezTests/LookatDeezTests.swift
@@ -0,0 +1,17 @@
+//
+//  LookatDeezTests.swift
+//  LookatDeezTests
+//
+//  Created by Phelps Merrell on 8/20/25.
+//
+
+import Testing
+@testable import LookatDeez
+
+struct LookatDeezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
